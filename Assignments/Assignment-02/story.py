48ffc8daae378c6c570f10a80966212d34d10f2c
samar <samarnasef80@gmail.com>
2018-02-04 22:28:54 +0200

    modify story.py

--- Assignments/Assignment-02/story.py
+++ Assignments/Assignment-02/story.py
@@ -1,3 +1,4 @@
+import re
 class person:
     mood = ('happy','tired','lazy')
     def __init__(self,name,money,health_rate):
@@ -10,11 +11,11 @@ class person:
     def sleep(self,hours):
         msg=''
         if hours == 7:
-            msg = mood[0]
+            msg = self.mood[0]
         elif hours < 7:
-            msg = mood[1]
+            msg = self.mood[1]
         elif hours > 7:
-            msg = mood[2] 
+            msg = self.mood[2] 
         return msg
     def eat(self,meals):
         msg=''
@@ -28,35 +29,68 @@ class person:
 
     def buy(self,items):
         if items == 1:
-            self.money - 10
+            self.money = self.money - 10
 class employee(person):
-    def __init__(self,id,car,email,salary,distance_to_work):
+    def __init__(self,id,carname,email,salary,distance_to_work):        
         self.id=id
-        self.car=car
-        self.email=email
+        self.carname=carname
+        match = re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$',email)
+        if match:
+            self.email=email
+            #print("valid email")    
+        else:
+            print("email not valid")
+            
         if salary >= 1000:
             self.salary=salary
         else:
             print("salary must be 1000 or more")
         self.distance_to_work=distance_to_work
-    
+        self.car1 = car(carname)
+
     def work(self,hours):
         msg=''
         if hours == 8:
-            msg = mood[0]
+            msg = self.mood[0]
         elif hours < 8:
-            msg = mood[1]
+            msg = self.mood[1]
         elif hours > 8:
-            msg = mood[2] 
+            msg = self.mood[2] 
         return msg
-    def drive(self,distance):
-        car.run(self.velocity,self.distance_to_work)
+    def drive(self,velocity):
+        self.car1.run(self.distance_to_work,velocity)
 
     def refuel(self,gas_amount=100):
-        self.fuelrate = self.fuelrate + gas_amount
+        self.car1.addfuelrate(gas_amount)
         
     def send_mail(self):
         pass
+class car:
+    def __init__(self,name,fuelrate=50,velocity=50):
+        self.name=name
+        if fuelrate >0 and fuelrate <100:
+            self.fuelrate=fuelrate
+        if velocity >0 and velocity<200:
+            self.velocity=velocity
+    def addfuelrate(self,gas_amount):
+        self.fuelrate += gas_amount
+    def stop(self,remain_distance):
+        self.velocity = 0
+        if remain_distance == -1:
+            print("you arrive the destination")
+        else:
+            print("remain distance : ",remain_distance)
+
+    def run(self,distance,velocity):
+        if self.fuelrate >0:
+            self.fuelrate = self.fuelrate -1
+            self.velocity = velocity
+            distance = distance -1
+        if(distance == 0):
+            self.stop(-1)
+        else:
+            self.stop(distance)
+
 class office:
     def __init__(self,name,employees):
         self.name=name
@@ -75,56 +109,4 @@ class office:
         pass
     def reward(self):
         pass
-class car:
-    def __init__(self,name,fuelrate,velocity):
-        self.name=name
-        if fuelrate >0 and fuelrate <100:
-            self.fuelrate=fuelrate
-        if velocity >0 and velocity<200:
-            self.velocity=velocity
-    def run(self,velocity,distance):
-        if self.fuelrate >0:
-            self.fuelrate = self.fuelrate -1
-            distance = distance - 1
-        velocity = self.velocity
-        if(distance == 0):
-            stop(-1)
-        else:
-            stop(distance)
-    def stop(self,remain_distance):
-        self.velocity = 0
-        if remain_distance == -1:
-            print("you arrive the destination")
-        else:
-            print("remain distance : ",remain_distance)
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
